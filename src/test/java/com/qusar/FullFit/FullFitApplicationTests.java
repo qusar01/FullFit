830048e6b00846618f80b88e6d3fb1d843f937b9
qusar01 <oskar.baranowski@interia.pl>
2024-03-20 16:25:50 +0100

    initial

---
+++ src/test/java/com/qusar/FullFit/FullFitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qusar.FullFit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullFitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
